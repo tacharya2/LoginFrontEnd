00a08d599f4bcd03ae6aced009a3dc3a044322b9
Tek Acharya <sarmakash430@gmail.com>
2023-08-31 11:10:00 -0400

    PageBeautify.1 : some cleaning

--- src/Terms.js
+++ src/Terms.js
@@ -4,12 +4,11 @@ import './AppContainer.css';
 
 function Terms() {
     return (
-        <div className='home'>
+        <div className='page-container'>
             <h2>Intra National Support Foundation of America </h2>
             {/* Add your content here */}
-            <h1>Software usage terms and conditions</h1>
+            <h2>Software usage terms and conditions</h2>
             <p>We are growing this page. Please visit it later </p>
-            <p className="log-in">Please <Link to="/Login">click here</Link> to log into your account</p>
         </div>
     );
 }
