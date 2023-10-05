735d0dc8db6e928d221fa2049d7eb248561e95f5
Tek Acharya <sarmakash430@gmail.com>
2023-10-04 21:36:39 -0400

    AttendanceDisplay.1 : succesfully displayed the children names in a dropdown for the attendance table

---
+++ src/AttendanceTable.js
@@ -0,0 +1,68 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+function AttendanceTable({ userId, selectedChildId }) {
+  const [attendanceData, setAttendanceData] = useState([]);
+  const [currentWeek, setCurrentWeek] = useState([]);
+
+  // Function to fetch attendance data
+  const fetchAttendanceData = async () => {
+    try {
+      const response = await axios.get(
+        `http://localhost:8080/api/attendance/${userId}/${selectedChildId}/getAttendance`
+      );
+      setAttendanceData(response.data);
+      console.log('Attendance =' + response.data);
+    } catch (error) {
+      console.error('Error fetching attendance data:', error);
+    }
+  };
+
+  useEffect(() => {
+    // Fetch attendance data when the component mounts or when selectedChildId changes
+    fetchAttendanceData();
+  }, [userId, selectedChildId]);
+
+  // Function to update the current week's range
+  const updateWeekRange = () => {
+    // Calculate the start and end date of the current week
+    const today = new Date();
+    const startOfWeek = new Date(today);
+    startOfWeek.setDate(today.getDate() - today.getDay()); // Start from Monday
+    const endOfWeek = new Date(today);
+    endOfWeek.setDate(startOfWeek.getDate() + 6); // End on Sunday
+
+    // Format the date range as needed (e.g., "MM/DD/YYYY - MM/DD/YYYY")
+    const formattedRange = `${startOfWeek.toLocaleDateString()} - ${endOfWeek.toLocaleDateString()}`;
+    setCurrentWeek(formattedRange);
+  };
+
+  useEffect(() => {
+    // Update the current week's range when the component mounts
+    updateWeekRange();
+  }, []);
+
+  return (
+    <div>
+      <h2>Attendance for Current Week: {currentWeek}</h2>
+      <table>
+        <thead>
+          <tr>
+            <th>Day</th>
+            <th>Attendance Status</th>
+          </tr>
+        </thead>
+        <tbody>
+          {attendanceData.map((entry) => (
+            <tr key={entry.attendanceId}>
+              <td>{entry.day}</td>
+              <td>{entry.attendanceStatus}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default AttendanceTable;
--- src/DummyPage.js
+++ src/DummyPage.js
@@ -1,67 +1,94 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import jwtDecode from 'jwt-decode';
+import axios from 'axios'; // Import axios for making API requests
 import { useAuthentication } from './AuthenticationContext';
-import { Navigate, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import ChildForm from './AddChild';
-import ParentInfo from './ParentInfo'
+import ParentInfo from './ParentInfo';
+import AttendanceTable from './AttendanceTable';
 
 function DummyPage() {
-    const { authenticated, setAuthenticated } = useAuthentication();
-    const navigate = useNavigate(); // Declare navigate
+  const { authenticated, setAuthenticated } = useAuthentication();
+  const navigate = useNavigate();
 
+  const decodedToken = jwtDecode(localStorage.getItem('jwtToken'));
+  const userId = decodedToken.userId;
 
-const decodedToken = jwtDecode(localStorage.getItem('jwtToken'));
-const userId = decodedToken.userId;
+  console.log("My Id ="+ userId);
 
+  // State to store the list of children and the selected child ID
+  const [children, setChildren] = useState([]); // Initialize with an empty array
+  const [selectedChildId, setSelectedChildId] = useState(null);
 
-    console.log('userId:', userId);
-    const token = localStorage.getItem('jwtToken');
-    if (token) {
-      const decodedToken = jwtDecode(token);
-      console.log('Decoded token:', decodedToken);
+  // Fetch the list of children associated with the parent
+  const fetchChildren = async () => {
+    try {
+      // Make an API call to fetch the children data for the current parent (userId)
+      const response = await axios.get(`http://localhost:8080/api/children/${userId}/myChild`);
+      const childNames = response.data.map((child)=> child.fullName);
 
-      if (decodedToken.userId) {
-        const userId = parseInt(decodedToken.userId, 10);
-        console.log('Parsed userId:', userId);
-      } else {
-        console.log('userId field not found in the token');
-      }
-    } else {
-      console.log('jwtToken not found in localStorage');
+        //Just an example on how to get and map the particular data from the response data.
+      const childIds = response.data.map((child) => child.age);
+      console.log(childIds);
+
+      setChildren(childNames); // Update the state with the fetched children data
+      console.log("myChild = " + childNames);
+    } catch (error) {
+      console.error('Error fetching children data:', error);
     }
-
-
-
-    useEffect(() => {
-    // Update the session start timestamp when the user interacts with the DummyPage
-    localStorage.setItem('sessionStart', new Date().getTime());
+  };
+  // Call fetchChildren when the component mounts
+  useEffect(() => {
+    fetchChildren();
   }, []);
-    const handleLogout = () => {
-        // Clear the authentication state and redirect to the Home page
-        localStorage.removeItem('jwtToken');
-        localStorage.removeItem('authenticated');
-        localStorage.removeItem('sessionStart');
-        setAuthenticated(false);
-        navigate('/Home'); // Redirect to the Home page
-      };
-  // If not authenticated, render nothing (or an appropriate message)
-  if (!authenticated) {
-    return null;
-  }
-    const handleChildAdded = (childInfo) => {
-      console.log('Child added:', childInfo);
-    }
+
+  const handleChildAdded = (childInfo) => {
+    console.log('Child added:', childInfo);
+    // After adding a child, you can choose to refresh the list of children
+    fetchChildren();
+  };
+  const handleLogout = () => {
+    // Clear the authentication state and redirect to the Home page
+    localStorage.removeItem('jwtToken');
+    localStorage.removeItem('authenticated');
+    localStorage.removeItem('sessionStart');
+    setAuthenticated(false);
+    navigate('/Home'); // Redirect to the Home page
+  };
+
+  const handleChildSelect = (childId) => {
+    // Set the selected child ID when a child is selected from the dropdown
+    setSelectedChildId(childId);
+  };
+
   // Render the DummyPage content for authenticated users
   return (
-    <div className='page-container'>
-      <h1>Intra-national Support Foundation of America </h1>
+    <div className="page-container">
+      <h1>Intra-national Support Foundation of America</h1>
       {/* Add your content here */}
       <h2>Please register your child</h2>
-      <p>A family can register maximum of 3 child</p>
+      <p>A family can register a maximum of 3 children</p>
       <ChildForm onChildAdded={handleChildAdded} userId={userId} />
-      <ParentInfo userId={userId} onLogout={handleLogout}/>
-      {/*<button onClick={handleLogout}>Logout</button> {/* Logout button */}
+      <ParentInfo userId={userId} onLogout={handleLogout} />
+
+      {/* Child selection dropdown */}
+      <h3>Select a Child:</h3>
+            <select
+              onChange={(e) => handleChildSelect(e.target.value)}
+              value={selectedChildId}
+            >
+              <option value="">Select a Child</option>
+              {children.map((childName, index) => (
+                <option key={index} value={index}>
+                  {childName}
+                </option>
+              ))}
+            </select>
+
+      {/* Pass userId, selectedChildId, and children to AttendanceTable */}
+      <AttendanceTable userId={userId} selectedChildId={selectedChildId} children={children} />
     </div>
   );
 }
+
 export default DummyPage;
